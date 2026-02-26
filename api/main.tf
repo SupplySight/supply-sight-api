e61ed489e7475646e1c5ffd44ab52de683e68cca
Ryan Virtue <113137446+RyanVSD@users.noreply.github.com>
2026-02-25 21:39:25 -0800

    add a dynamo table

--- api/main.tf
+++ api/main.tf
@@ -1,5 +1,21 @@
 data "aws_caller_identity" "current" {}
 
+resource "aws_dynamodb_table" "test_table" {
+  name           = "test_table"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+
+  attribute {
+    name = "attr1"
+    type = "S"
+  }
+
+  attribute {
+    name = "attr2"
+    type = "B"
+  }
+}
+
 output "account_id" {
   value = data.aws_caller_identity.current.account_id
 }
