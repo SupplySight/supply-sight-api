d9de43af932732e94fcf11887ca69ae8ae3ef070
ryancv <ryancvirtue@gmail.com>
2026-02-25 21:52:08 -0800

    remove table

--- api/main.tf
+++ api/main.tf
@@ -1,12 +1,1 @@
 data "aws_caller_identity" "current" {}
-
-resource "aws_dynamodb_table" "test_table" {
-  name         = "test_table"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "id"
-
-  attribute {
-    name = "id"
-    type = "N"
-  }
-}
